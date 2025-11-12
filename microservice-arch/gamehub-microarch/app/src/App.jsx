7a6699e51b8529edf630e54e3058a17a53a1cdd6
tejubagalad <tejashwinibagalad1599@gmail.com>
2025-11-12 18:23:35 +0530

    check if the changes work

--- microservice-arch/gamehub-microarch/app/src/App.jsx
+++ microservice-arch/gamehub-microarch/app/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
           Welcome to the PlayHive!
         </h1>
         <p className="text-secondary mb-4">
-          Choose your game below and have Fun!!!
+          Play hard. Win smart
         </p>
 
         <div className="row justify-content-center g-3 w-100 px-3">
